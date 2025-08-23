916e8199af4a3a3bbc861d7176d12382baced77b
Ashish <kumarmaliashish0@gmail.com>
2025-08-23 17:58:12 +0530

    Add favicon icon in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         {/* GTM Script */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
-import Ctabutton from './components/Ctabutton'
 
 export default function Home() {
   return (
